e835dc6bbfde783a90bf530acc7eb3a4dbe09a7a
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-06-30 10:49:21 -0500

    Version 2.2

--- app/script.js
+++ app/script.js
@@ -1,5 +1,3 @@
-// JavaScript para controle de interatividade
-
 // Toggle para tema claro/escuro
 const themeButton = document.getElementById('toggle-theme');
 const body = document.body;
@@ -8,6 +6,5 @@ const container = document.querySelector('.container');
 themeButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     container.classList.toggle('dark-mode');
-    // Alterna entre ícones de sol e lua
     themeButton.innerHTML = body.classList.contains('dark-mode') ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
 });
